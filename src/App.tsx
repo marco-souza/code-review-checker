a7d36849b0e4e08b4f79f9526df099fb1ec1faba
Marco Antônio <ma.souza.junior@gmail.com>
2024-08-26 17:11:02 -0300

    fea: create initial review pop-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import type { Component } from 'solid-js';
+import { type Component, createSignal, For, Show } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Popover } from "@kobalte/core/popover";
+import { Button } from "@kobalte/core/button";
+import { classes } from "./utils";
+
+type Item = {
+  checked: boolean;
+  name: string;
+};
+
+// list of important things to check in a code review
+const itemsToReview: string[] = [
+  "Tests",
+  "Security",
+  "Performance",
+  "Scalability",
+  "Reliability",
+  "Maintainability",
+  "Readability",
+];
+
+const [checklist, setChecklist] = createSignal<Item[]>(
+  itemsToReview.map((name) => ({ name, checked: false }) as Item),
+);
+
+const isAllChecked = () => checklist().every((item) => item.checked);
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Popover>
+      <Popover.Trigger class="btn btn-primary">Review</Popover.Trigger>
+
+      <Popover.Portal>
+        <Popover.Content class="popover__content">
+          <Popover.Arrow />
+          <div class="popover__header">
+            <Popover.Title class="popover__title">
+              Code Review Checker
+            </Popover.Title>
+
+            <Popover.CloseButton class="popover__close-button">
+              X
+            </Popover.CloseButton>
+          </div>
+
+          <Popover.Description class="popover__description grid gap-2">
+            <For each={checklist()}>
+              {(item, idx) => (
+                <div
+                  class="popover__item flex gap-1"
+                  onClick={() =>
+                    setChecklist((list) =>
+                      list.map((i, index) =>
+                        index !== idx() ? i : { ...i, checked: !i.checked }
+                      )
+                    )}
+                >
+                  <input
+                    type="checkbox"
+                    checked={item.checked}
+                  />
+                  <label>{item.name}</label>
+                </div>
+              )}
+            </For>
+            <Button
+              class={classes(
+                "btn btn-primary btn-sm w-full",
+                !isAllChecked() && "btn-disabled",
+              )}
+              disabled={!isAllChecked()}
+              onClick={() => alert("Review done! ✅")}
+            >
+              Done ✅
+            </Button>
+          </Popover.Description>
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 render(
   () => <App />,
   (() => {
-    const app = document.createElement('div');
-    app.style.flex = '1';
+    const app = document.createElement("div");
+
+    app.style.flex = "1";
+    app.style.position = "fixed";
+    app.style.bottom = "20px";
+    app.style.right = "20px";
+    app.style.zIndex = "9999";
+
     document.body.append(app);
     return app;
   })(),
