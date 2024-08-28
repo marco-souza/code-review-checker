d657d0fb141d5e202ca46b264bf9823a38acff1a
Marco Antônio <ma.souza.junior@gmail.com>
2024-08-28 12:35:40 -0300

    ref: move Checklist Popover to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { type Component, For } from "solid-js";
+import type { Component } from "solid-js";
 
 import { Popover } from "@kobalte/core/popover";
-
-import clippyCheck from "../static/clippy-check.gif";
-import clippyJump from "../static/clippy-jump.gif";
-import { SettingsDialog } from "./components/SettingsDialog";
 import {
-  type Item,
-  checklist,
-  isAllChecked,
-  toggleCheck,
-} from "./domain/checklist";
+  PopoverBody,
+  PopoverHeader,
+  PopoverIcon,
+} from "./components/ChecklistPopover";
 
 const App: Component = () => {
   return (
@@ -35,53 +30,3 @@ const App: Component = () => {
 };
 
 export default App;
-
-const PopoverBody: Component = () => {
-  return (
-    <For each={checklist()}>
-      {(item, idx) => <PopoverItem id={idx()} item={item} />}
-    </For>
-  );
-};
-
-const PopoverItem: Component<{ id: number; item: Item }> = ({ id, item }) => {
-  return (
-    <div
-      class="flex gap-1"
-      onClick={() => toggleCheck(id)}
-      onKeyPress={() => toggleCheck(id)}
-    >
-      <input type="checkbox" class="checkbox" checked={item.checked} />
-
-      <label>{item.name}</label>
-    </div>
-  );
-};
-
-const PopoverHeader: Component = () => {
-  return (
-    <div class="flex gap-4 align-middle justify-center">
-      <Popover.Title class="text-base font-medium text-gray-900">
-        Code Review Checker
-      </Popover.Title>
-
-      <div>
-        <SettingsDialog />
-
-        <Popover.CloseButton class="btn btn-ghost btn-xs" title="Close">
-          X
-        </Popover.CloseButton>
-      </div>
-    </div>
-  );
-};
-
-const PopoverIcon: Component = () => {
-  return (
-    <img
-      src={!isAllChecked() ? clippyJump : clippyCheck}
-      alt="Code Review"
-      class="h-16"
-    />
-  );
-};
---
+++ src/components/ChecklistPopover.tsx
@@ -0,0 +1,65 @@
+import { type Component, For } from "solid-js";
+
+import { Popover } from "@kobalte/core/popover";
+
+import clippyCheck from "#/static/clippy-check.gif";
+import clippyJump from "#/static/clippy-jump.gif";
+import { SettingsDialog } from "@/components/SettingsDialog";
+import {
+  type Item,
+  checklist,
+  isAllChecked,
+  toggleCheck,
+} from "@/domain/checklist";
+
+const PopoverBody: Component = () => {
+  return (
+    <For each={checklist()}>
+      {(item, idx) => <PopoverItem id={idx()} item={item} />}
+    </For>
+  );
+};
+
+const PopoverItem: Component<{ id: number; item: Item }> = ({ id, item }) => {
+  return (
+    <div
+      class="flex gap-1"
+      onClick={() => toggleCheck(id)}
+      onKeyPress={() => toggleCheck(id)}
+    >
+      <input type="checkbox" class="checkbox" checked={item.checked} />
+
+      <label>{item.name}</label>
+    </div>
+  );
+};
+
+const PopoverHeader: Component = () => {
+  return (
+    <div class="flex gap-4 align-middle justify-center">
+      <Popover.Title class="text-base font-medium text-gray-900">
+        Code Review Checker
+      </Popover.Title>
+
+      <div>
+        <SettingsDialog />
+
+        <Popover.CloseButton class="btn btn-ghost btn-xs" title="Close">
+          X
+        </Popover.CloseButton>
+      </div>
+    </div>
+  );
+};
+
+const PopoverIcon: Component = () => {
+  return (
+    <img
+      src={!isAllChecked() ? clippyJump : clippyCheck}
+      alt="Code Review"
+      class="h-16"
+    />
+  );
+};
+
+export { PopoverBody, PopoverHeader, PopoverIcon };
