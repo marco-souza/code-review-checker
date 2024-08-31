9e992f6feb1d800debb7624178b571da49c32a2f
Marco Antônio <ma.souza.junior@gmail.com>
2024-08-31 10:57:40 -0300

    fix: modal styles

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -8,15 +8,20 @@ export function SettingsDialog() {
       <Dialog.Portal>
         <Dialog.Overlay class="fixed inset-0 z-50 bg-gray-900 bg-opacity-80 animate-fade-in" />
 
-        <Dialog.Content>
-          <div class="modal-box">
-            <Dialog.CloseButton>Close</Dialog.CloseButton>
-            <Dialog.Title>Code Review Settings</Dialog.Title>
+        <div class="fixed inset-0 z-50 flex items-center justify-center">
+          <Dialog.Content class="modal-box grid gap-4">
+            <div class="flex flex-row justify-between">
+              <Dialog.Title class="h1 text-lg">
+                Code Review Settings
+              </Dialog.Title>
+              <Dialog.CloseButton class="left">X</Dialog.CloseButton>
+            </div>
+
             <Dialog.Description>
               Configure your code review checklist
             </Dialog.Description>
-          </div>
-        </Dialog.Content>
+          </Dialog.Content>
+        </div>
       </Dialog.Portal>
     </Dialog>
   );
